497db35e6490893afff654d1afb6d5b1fed5c282
Ids Osinga <ip.osinga@alfa-college.nl>
2024-09-24 14:09:06 +0200

    v2024-09-24

--- eenheden/eenhomreken.js
+++ eenheden/eenhomreken.js
@@ -1,7 +1,20 @@
-function hallo(){
-    alert("HALLO!!!")
+let dimension = document.getElementById("dimselect");
+let opg = document.getElementById("opginput");
+let opgeenh = document.getElementById("opgeenh");
+
+let antw = document.getElementById("antwinput");
+let antweenh = document.getElementById("antweenh");
+
+function nieuw(){
+    //de gekozen dimensie moet ik weten
+    if(dimension.value == 0) {
+        alert("Kies een dimensie stumper!");
+    } else {
+        //hier moet nu geprogrammeerd worden
+        alert("De gekozen dimensie = " + dimension.value);
+    }
 }
 
-function welkom(){
-    alert("WELKOM!!!")
+function check(){
+    alert("Check!!!")
 }
