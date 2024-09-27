ffce2b3c41f1967489c16608b9d4b3825a941f32
Ids Osinga <ip.osinga@alfa-college.nl>
2024-09-27 11:52:51 +0200

    v2024-09-27_1

--- eenheden/eenhomreken.js
+++ eenheden/eenhomreken.js
@@ -1,17 +1,28 @@
+const eenheden = ["mm", "cm", "dm", "m", "dam", "hm", "km"]
+//index             0     1     2    3     4      5     6
+
 let dimension = document.getElementById("dimselect");
 let opg = document.getElementById("opginput");
 let opgeenh = document.getElementById("opgeenh");
-
 let antw = document.getElementById("antwinput");
 let antweenh = document.getElementById("antweenh");
 
 function nieuw(){
     //de gekozen dimensie moet ik weten
     if(dimension.value == 0) {
-        alert("Kies een dimensie stumper!");
+        alert("Kies een dimensie a.u.b.!");
     } else {
         //hier moet nu geprogrammeerd worden
-        alert("De gekozen dimensie = " + dimension.value);
+        //genereer een willekeurig getal tussen 0 en 6, en neem
+        //die als index voor eenheden[]
+        let randomnmbr = Math.floor(Math.random() * 7);
+        //console.log(randomnmbr + " " + eenheden[randomnmbr]);
+        if(dimension.value == 1)
+            opgeenh.innerHTML = eenheden[randomnmbr];
+        else
+            opgeenh.innerHTML = eenheden[randomnmbr] + "<sup>" + dimension.value + "</sup>";
+        //console.log("De gekozen dimensie = " + dimension.value);
+        //console.log("het eerste element uit eenheden = " + eenheden[0]);
     }
 }
 
