c43b7da76b32294e260bbab066d2db0349301450
Ids Osinga <ip.osinga@alfa-college.nl>
2024-10-08 13:17:16 +0200

    v2024-10-08

--- eenheden/eenhomreken.js
+++ eenheden/eenhomreken.js
@@ -7,33 +7,31 @@ let opgeenh = document.getElementById("opgeenh");
 let antw = document.getElementById("antwinput");
 let antweenh = document.getElementById("antweenh");
 
-function nieuw(){
+function nieuw() {
+    antw.value = "";
     //de gekozen dimensie moet ik weten
-    if(dimension.value == 0) {
+    if (dimension.value == 0) {
         alert("Kies een dimensie a.u.b.!");
     } else {
         //hier moet nu geprogrammeerd worden
         //genereer een willekeurig getal tussen 0 en 6, en neem
         //die als index voor eenheden[]
         let randomnmbr = Math.floor(Math.random() * 7);
+        let randomnmbr2 = Math.floor(Math.random() * 7);
         //console.log(randomnmbr + " " + eenheden[randomnmbr]);
-        if(dimension.value == 1)
+        if (dimension.value == 1){
             opgeenh.innerHTML = eenheden[randomnmbr];
-        else
+            antweenh.innerHTML = eenheden[randomnmbr2];
+        }
+        else {
             opgeenh.innerHTML = eenheden[randomnmbr] + "<sup>" + dimension.value + "</sup>";
-        
-        
-        
-        
-            let randomnmbr2 = Math.floor(Math.random() * 7);
-            console.log("randomnmbr2 = " + randomnmbr2 + " met eenheid = " + eenheden[randomnmbr2]);
             antweenh.innerHTML = eenheden[randomnmbr2] + "<sup>" + dimension.value + "</sup>";
-            
-            let randomdec = (Math.random() * 1000).toFixed(3);
-            opg.value = randomdec;
+        }
+        let randomdec = (Math.random() * 1000).toFixed(3);
+        opg.value = randomdec;
     }
 }
 
-function check(){
+function check() {
     alert("Check!!!")
 }
